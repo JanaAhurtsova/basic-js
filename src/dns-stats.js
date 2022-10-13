4afd5c89d7713b3e5cbfd3235b337ea1c91bab55
Jana Ahurtsova <byjankaaa@gmail.com>
2022-10-13 20:19:34 +0300

    feat: add solution to dns

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+
+  domains.forEach(domain => {
+   let arr = domain.split('.').reverse();
+   let key = '';
+    for(let i = 0; i < arr.length; i++) {
+      key += `.${arr[i]}`;
+      if(Object.hasOwn(obj, key)) {
+        obj[key]++
+      } else {
+        obj[key] = 1
+      }
+    }
+  });
+  return obj;
 }
 
 module.exports = {
